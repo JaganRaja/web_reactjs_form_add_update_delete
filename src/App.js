e063fedbe13ccf7283044ad22489134de632701b
JaganRaja <jaganraja479@gmail.com>
2022-09-02 13:53:45 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import ListDetails from "./ListDetails.js";
 
 function App() {
+  const [personalObj, setPersonalObj] = useState({});
+  const [fullDetails, setFullDetails] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setPersonalObj((previousState) => {
+      return { ...previousState, [name]: value };
+    });
+  };
+
+  console.log("personalObj", personalObj);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let id_number = fullDetails.length + 1;
+    setFullDetails((previousState) => {
+      return [
+        ...previousState,
+        {
+          ...personalObj,
+          id: personalObj.phonenumber + "_" + id_number,
+        },
+      ];
+    });
+    setPersonalObj((previousState) => {
+      return {};
+    });
+  };
+
+  console.log("fullDetails", fullDetails);
+
+  const handleEdit = (obj) => {
+    console.log("Edit obj", obj);
+    setPersonalObj(obj);
+    setIsEdit(true);
+  };
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    setFullDetails((previousState) => {
+      const newState = previousState.map((details) => {
+        if (details.id === personalObj.id) {
+          console.log("...details", details);
+          return { ...details, ...personalObj };
+        }
+
+        return details;
+      });
+      return newState;
+    });
+    setPersonalObj({});
+    setIsEdit(false);
+  };
+
+  const handleDelete = (obj) => {
+    //console.log("obj", obj);
+    setFullDetails((previousState) =>
+      previousState.filter((details) => {
+        return details.id !== obj.id;
+      })
+    );
+  };
+
+  // const handleDelete = (obj) => {
+  //   console.log("obj", obj);
+  //   setFullDetails((previousState) => {
+  //     return previousState.filter((details) => {
+  //       return details.id !== obj.id;
+  //     });
+  //   });
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // container
+    <div className="container">
+      {/* form */}
+      {/* onSubmit={handleSubmit} */}
+      <form>
+        <label htmlFor="fullname">First Name</label>
+        <input
+          type="text"
+          id="fullname"
+          name="fullname"
+          value={personalObj.fullname || ""}
+          onChange={handleInputChange}
+          placeholder="your name"
+          required
+        ></input>
+        <label htmlFor="email">Last Name</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={personalObj.email || ""}
+          onChange={handleInputChange}
+          placeholder="sample@mail.com"
+          required
+        ></input>
+        <label htmlFor="phonenumber">Last Name</label>
+        <input
+          type="tel"
+          id="phonenumber"
+          name="phonenumber"
+          value={personalObj.phonenumber || ""}
+          onChange={handleInputChange}
+          pattern="[0-9]{10}"
+          placeholder="1234567890"
+          maxLength="10"
+          minLength="10"
+          required
+        ></input>
+
+        {isEdit ? (
+          <input type="submit" value="Update" onClick={handleUpdate}></input>
+        ) : (
+          <input type="submit" value="Submit" onClick={handleSubmit}></input>
+        )}
+      </form>
+      <ListDetails
+        data={fullDetails}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 }
---
+++ src/ListDetails.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./ListDetails.css";
+
+function ListDetails({ data, handleEdit, handleDelete }) {
+  return (
+    <div className="ListDetailsContainer">
+      <h4>please see what you have added:</h4>
+      {data &&
+        data.map((item) => {
+          return (
+            <div key={item.id} className="details">
+              <div className="details_left">
+                <p>Name: {item.fullname}</p>
+                <p>Email: {item.email}</p>
+                <p>Phone Number: {item.phonenumber}</p>
+              </div>
+              <div className="details_right">
+                <button type="button" onClick={() => handleEdit(item)}>
+                  Edit
+                </button>
+                <button type="button" onClick={() => handleDelete(item)}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export default ListDetails;
